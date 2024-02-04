36eb0c23a11a77437caec079c7556a0449939366
Aibad <143527312+Aibad-khan@users.noreply.github.com>
2024-02-04 14:02:46 +0530

    Add files via upload

---
+++ smallbazaar/smallbazaar/about.php
@@ -0,0 +1,74 @@
+<?php
+require ("includes/common.php");
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Small Bazaar | Online Shopping Site for Men</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href='https://fonts.googleapis.com/css?family=Delius Swash Caps' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Andika' rel='stylesheet'>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body style="overflow-x:hidden; padding-bottom:100px;">
+  <?php
+        include 'includes/header_menu.php';
+    ?>
+  <div>
+    <div class="container mt-5 ">
+      <div class="row justify-content-around">
+        <div class="col-md-5 mt-3">
+          <h3 class="text-warning pt-3 title">Who am i?</h3>
+          <hr />
+          
+          <p class="mt-2">Hello friends,my name is Aibad Khan  and i am pre final year student at Jaypee Institute Of Information Technoloy.i am a passionate programmer who loves coding and solving real life problems.</p>
+        </div>
+        <div class="col-md-5 mt-3">
+          <span class="text-warning pt-3">
+            <h1 class="title">LIVE SUPPORT</h1>
+            <h3>24 hours|7 days a week| 365 days a year Live Technical Support</h3>
+          </span>
+          <hr>
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container pb-3">
+  </div>
+  <div class="container mt-3 d-flex justify-content-center card pb-3 col-md-6">
+
+    <form class="col-md-12" action="" method="POST" name="_next">
+      <h3 class="text-warning pt-3 title mx-auto">Contact Form</h3>
+      <div class="form-group">
+        <label for="exampleFormControlInput1">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter Your Email"
+          name="email">
+      </div>
+
+      <div class="form-group">
+        <label for="exampleFormControlTextarea1">Message</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" name="message" rows="5"></textarea>
+      </div>
+      <input type="hidden" name="_next" value="http://localhost/foody/about.php" />
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+
+
+  </div>
+  <!--footer -->
+  <?php include 'includes/footer.php'?>
+  <!--footer end-->
+
+
+</body>
+</html>
---
+++ smallbazaar/smallbazaar/includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Small Bazaar | Contact Us: +91 1234567098</b></span></div>
+</footer>
+    
